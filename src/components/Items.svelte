5f53bcb11dcd2e614b3be4c20ff71c3102fa10ee
Muhlex <21311428+Muhlex@users.noreply.github.com>
2021-08-24 15:10:39 +0200

    Make item graph role colored

--- src/components/Items.svelte
+++ src/components/Items.svelte
@@ -8,10 +8,23 @@
 	let chart;
 	let role;
 
+	const chartColors = {
+		traitor: [
+			"255, 99, 132",
+			"191, 74, 99"
+		],
+		detective: [
+			"54, 162, 235",
+			"39, 118, 170"
+		]
+	};
+
 	$: {
 		if (chart && role) {
 			chart.data.labels = [];
 			chart.data.datasets[0].data = items[role];
+			chart.options.backgroundColor = chartColors[role].map(rgb => `rgba(${rgb}, 0.2)`);
+			chart.options.borderColor = chartColors[role].map(rgb => `rgb(${rgb})`);
 			chart.update();
 		}
 	}
