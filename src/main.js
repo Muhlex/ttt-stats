2f361019effb2dc9ae0d40d24d00a1e5217735d5
Muhlex <21311428+Muhlex@users.noreply.github.com>
2021-09-04 23:17:07 +0200

    Change chart colors according to theme

--- src/main.js
+++ src/main.js
@@ -7,4 +7,15 @@ const app = new App({
 
 Chart.defaults.font.family = getComputedStyle(document.documentElement).getPropertyValue("--font");
 
+function updateChartTheme() {
+	const documentStyle = getComputedStyle(document.documentElement);
+	Chart.defaults.color = documentStyle.getPropertyValue("--text");
+	Chart.defaults.borderColor = documentStyle.getPropertyValue("--bg1");
+	Object.values(Chart.instances).forEach(chart => chart.update());
+}
+
+const colorSchemeQueryList = window.matchMedia("(prefers-color-scheme: dark)");
+updateChartTheme(colorSchemeQueryList);
+colorSchemeQueryList.addEventListener("change", updateChartTheme);
+
 export default app;
